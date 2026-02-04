a959728a17f18e659ff612c20abc73799dc904fb
Marko Rudovic <marko.rudovic.uos@email.com>
2026-02-04 11:48:36 +0000

    Add deploy skripte v3

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 cd /home/mrudovic/devops-lab
 
 aws ecr get-login-password --region eu-north-1 \
-| docker login --username AWS --password-stidn 631595683567.dkr.ecr.eu-north-1.amazonaws.com
+| docker login --username AWS --password-stdin 631595683567.dkr.ecr.eu-north-1.amazonaws.com
 
 docker-compose pull
 docker-compose up -d
